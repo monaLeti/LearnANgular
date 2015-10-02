cf8e1fdaf83b1211e43d9894f1c3cabbabd44b1c
monaLeti <you@example.com>
2015-10-02 10:33:09 +0200

    Tutorial 6

--- directory.js
+++ directory.js
@@ -1,7 +1,14 @@
 angular.module('directoryApp', [])
-  .controller('directoryController', function($scope){
-    $scope.list = [{name: 'Scott', age: 29},
+  .controller('directoryController', function(){
+    var dirList = this;
+    dirList.list = [{name: 'Scott', age: 29},
       {name: 'Leti', age:30},
       {name: 'Laura', age:13},
       {name: 'Paco', age:33}];
+
+      dirList.addPerson = function (){
+        dirList.list.push({name: dirList.name, age: dirList.age})
+        dirList.name='';
+        dirList.age = 0;
+      };
   });
