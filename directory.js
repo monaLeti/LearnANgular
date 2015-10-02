a4fe3325cc82db94903dbc4e012704c563ed953c
monaLeti <you@example.com>
2015-10-02 10:53:37 +0200

    Tutorial 8

--- directory.js
+++ directory.js
@@ -1,6 +1,8 @@
 angular.module('directoryApp', [])
   .controller('directoryController', function(){
     var dirList = this;
+    dirList.toggle = false;
+
     dirList.list = [{name: 'Scott', age: 29},
       {name: 'Leti', age:30},
       {name: 'Laura', age:13},
